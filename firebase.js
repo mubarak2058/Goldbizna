2616af6bd3e2e0a61d8a1e89e8906477be1920f0
@mubarakbizna <mubarakjumamasoud@gmail.com>
2026-08-02 22:38:05 +0300

    Create firebase.js
    Signed-off-by: @mubarakbizna <mubarakjumamasoud@gmail.com>

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Firebase Configuration
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA8XB6aY45-yHT0uZ6NUhptMoNkYqgwqOs",
+  authDomain: "goldbizna.firebaseapp.com",
+  projectId: "goldbizna",
+  storageBucket: "goldbizna.firebasestorage.app",
+  messagingSenderId: "559981054862",
+  appId: "1:559981054862:web:0635c689607d3baa3272df"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
